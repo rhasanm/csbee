d68ee185ef73c752ee9a7d20ae85334788cb617c
Rakibul Hasan <mr.hasan@banglalink.net>
2024-06-21 15:19:07 +0600

    schema scrollbar init

--- src/ui.rs
+++ src/ui.rs
@@ -1,26 +1,23 @@
-use ratatui::{
-    style::{Color, Modifier, Style}, widgets::{Block, List, ListDirection, ListItem}, Frame
-};
+use ratatui::{prelude::*, Frame};
+use schema::schema_widget;
+
+mod schema;
 
 use crate::app::App;
 
 pub fn ui(frame: &mut Frame, app: &App) {
     let schema = app.df.schema();
+    let area = frame.size();
 
-    let items: Vec<_> = schema
-        .iter()
-        .map(|(key, val)| {
-            let formatted_str = format!("{}: {:?}", key, val);
-            ListItem::new(formatted_str)
-        })
-        .collect();
+    let (schema_widget, schema_scrollbar, mut schema_scrollbar_state)  = schema_widget(schema);
 
-    let widget = List::new(items)
-        .block(Block::bordered().title("Schema"))
-        .style(Style::default().fg(Color::White))
-        .highlight_style(Style::default().add_modifier(Modifier::ITALIC))
-        .highlight_symbol(">>")
-        .repeat_highlight_symbol(true)
-        .direction(ListDirection::TopToBottom);
-    frame.render_widget(widget, frame.size())
+    frame.render_widget(schema_widget, area);
+    frame.render_stateful_widget(
+        schema_scrollbar,
+        area.inner(&Margin {
+            vertical: 1,
+            horizontal: 0,
+        }),
+        &mut schema_scrollbar_state,
+    );
 }
